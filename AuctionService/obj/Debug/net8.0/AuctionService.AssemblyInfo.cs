4d8d15dff8e5046a289bda5f03c2e4bbd0dcc99b
peterdinis <petter1@pobox.sk>
2025-09-27 16:09:53 +0200

    fix: Comment one thing in AuctionController

--- AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a3b21b959027ac203668d157fdad497223eb6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe15c5886e5fdcb31d6c33a6582aff2ee541d4c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
