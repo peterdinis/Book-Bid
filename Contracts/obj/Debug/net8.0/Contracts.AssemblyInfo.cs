183ab98bb0586c33bd70d22ea763c3a892848a96
peterdinis <petter1@pobox.sk>
2025-10-04 21:33:55 +0200

    Fixing merge issue

--- Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65476d799ded23bf7a895b7bd6ae4c4c3507626a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1e213b0f6e8ec50dfdcf28d9f413f8e8446eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0811046f44f655c90105071c240e6cab363b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1e213b0f6e8ec50dfdcf28d9f413f8e8446eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
